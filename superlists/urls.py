1372cca221fa8886fa14d0a5eeddfb50e8956e74
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-05-03 20:21:26 -0300

    Ciclo:

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path as url
 from lists import views
 
 urlpatterns = [
