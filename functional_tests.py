1dd8b3f9cfa4a96c9dbdb57e66bb7683a919b91c
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-05-03 20:47:07 -0300

    Criando BD de Produção

--- functional_tests.py
+++ functional_tests.py
@@ -11,8 +11,28 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self): 
-        ...
+    
+        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        self.browser.get('http://localhost:8000')
+
+        # Ela nota que o título da página menciona TODO
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a entrar com um item TODO imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
+
         # Quando ela aperta enter, a página atualiza, e mostra a lista
         # "1: Estudar testes funcionais" como um item da lista TODO
         inputbox.send_keys(Keys.ENTER)
@@ -36,12 +56,6 @@ class NewVisitorTest(unittest.TestCase):
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
 
-
-        
 if __name__ == '__main__':
     unittest.main()
