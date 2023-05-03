c7c7f5d90ee88aa32087b1a4a7856cf794a6b359
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-05-03 20:40:43 -0300

    Redirecionando

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from lists.models import Item
+
 def home_page(request):
-    new_item_text = ''
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
+        return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text
-    })
+    return render(request, 'home.html')
 
 def test_only_saves_items_when_necessary(self):
     self.client.get('/')
