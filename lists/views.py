6e0aff410c2c0239d465e0346cda4c83408b2f00
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-05-03 20:23:14 -0300

    Refatorando

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
 
 from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import render
+
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
