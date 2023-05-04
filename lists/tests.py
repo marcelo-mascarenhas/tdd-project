d1df18ecab98379684d6532e8eae4808e9ecb1e9
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-05-03 21:02:09 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 3

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,5 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
-from lists.views import home_page
-from django.db import models
 from lists.models import Item
 
 class HomePageTest(TestCase):
@@ -11,7 +7,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
+    
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
 
@@ -24,16 +20,12 @@ class HomePageTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+    
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -53,7 +45,12 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'O primeiro item')
         self.assertEqual(second_saved_item.text, 'O segundo item')
 
+
 class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
