19688bfa51ad405ba7123d41c98fe3d5f0c79d7a
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-06-05 20:02:55 -0300

    Testando o envio de email: redirecionando para o home

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
+from django.test import TestCase
+
+class SendLoginEmailViewTest(TestCase):
+
+    def test_redirects_to_home_page(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+        self.assertRedirects(response, '/')
+
+
 
 class HomePageTest(TestCase):
 
