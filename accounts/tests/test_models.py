33a73005ab62e3f198dacf9c43941e8dfcac12df
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-06-05 20:04:03 -0300

    Test Double manual

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -21,3 +21,27 @@ class TokenModelTest(TestCase):
         token1 = Token.objects.create(email='a@b.com')
         token2 = Token.objects.create(email='a@b.com')
         self.assertNotEqual(token1.uid, token2.uid)
+
+
+class SendLoginEmailViewTest(TestCase):
+    
+    def test_sends_mail_to_address_from_post(self):
+        self.send_mail_called = False
+
+        def fake_send_mail(subject, body, from_email, to_list):
+            self.send_mail_called = True
+            self.subject = subject
+            self.body = body
+            self.from_email = from_email
+            self.to_list = to_list
+
+        accounts.views.send_mail = fake_send_mail
+
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        self.assertTrue(self.send_mail_called)
+        self.assertEqual(self.subject, 'Your login link for Superlists')
+        self.assertEqual(self.from_email, 'noreply@superlists')
+        self.assertEqual(self.to_list, ['edith@example.com'])
