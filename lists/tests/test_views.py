169c5e579a90fc0e526b95c439ea0145f1559f46
Marcelo Mascarenhas Ribeiro de Araújo <mmra@pop-os.localdomain>
2023-06-05 19:44:12 -0300

    Entrada inválida não deve ser salva no BD

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -54,7 +54,10 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
-
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 class NewItemTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
